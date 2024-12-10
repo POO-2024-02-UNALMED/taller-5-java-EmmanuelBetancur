d93dd226d52d6f75d60eea6244bbc466d31c4c87
EmmanuelBetancur <ebetancuru@unal.edu.co>
2024-12-10 01:18:17 -0500

    Create Zoologico.java

---
+++ src/main/java/gestion/Zoologico.java
@@ -0,0 +1,50 @@
+package gestion;
+import java.util.ArrayList;
+
+//Creacion de clase y sus atributos, constructores
+
+public class Zoologico {
+    private String nombre;
+    private String ubicacion;
+    private ArrayList<Zona> zonas;
+
+    public Zoologico(String nom, String ubi){
+        this.nombre = nom;
+        this.ubicacion = ubi;
+        this.zonas = new ArrayList<Zona>();
+    } 
+
+    //Creacion de metodos get
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public String getUbicacion(){
+        return this.ubicacion;
+    }
+
+    //Creacion de metodos set
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setUbicacion(String ubicacion){
+        this.ubicacion = ubicacion;
+    }
+
+    //creacion de metodos agregar zonas y cantidad total
+
+    public void agregarZonas(String nombre){
+        Zona nzona =  new Zona(nombre, this);
+        zonas.add(nzona);
+    }
+
+    public int cantidadTotalAnimales(){
+        int nanimales = 0;
+        for (Zona zona: zonas) {
+            nanimales += zona.cantidadAnimales();
+                }  return nanimales;
+        }
+    }
